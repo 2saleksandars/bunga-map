3d5a2ef9c4a5ab841283870a3bd1058918fef6ad
2saleksandars <2saleksandars@gmail.com>
2026-04-07 13:34:37 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -9,11 +9,21 @@ export default function Home() {
     mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 
     const map = new mapboxgl.Map({
-      container: mapRef.current,
-      style: "mapbox://styles/mapbox/streets-v12",
-      center: [13.405, 52.52],
-      zoom: 12,
-    });
+  container: mapRef.current,
+
+  style: "mapbox://styles/mapbox/navigation-night-v1", // 🎨 Farbe
+
+  center: [13.405, 52.52],
+  zoom: 12,
+
+  maxBounds: [
+    [13.0884, 52.3383],
+    [13.7612, 52.6755]
+  ],
+
+  minZoom: 10,
+  maxZoom: 16
+});
 
     return () => map.remove();
   }, []);
