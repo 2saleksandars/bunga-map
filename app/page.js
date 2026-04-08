ec75e67eade22d324f1a33d854e137fe2ef68cb7
2saleksandars <2saleksandars@gmail.com>
2026-04-08 09:26:57 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -13,15 +13,32 @@ export default function Home() {
       zoom: 12,
     });
 
-    // 🔥 FIX: wartet bis Map wirklich ready ist
     setTimeout(() => {
-      console.log("MAP READY");
 
-      new mapboxgl.Marker()
-        .setLngLat([13.405, 52.52])
-        .addTo(map);
+  const el = document.createElement("div");
+  el.style.width = "40px";
+  el.style.height = "40px";
+  el.style.borderRadius = "12px";
+  el.style.background = "white";
+  el.style.display = "flex";
+  el.style.alignItems = "center";
+  el.style.justifyContent = "center";
+  el.style.boxShadow = "0 4px 12px rgba(0,0,0,0.3)";
 
-    }, 1000);
+  const inner = document.createElement("div");
+  inner.style.width = "30px";
+  inner.style.height = "30px";
+  inner.style.backgroundImage = "url('/gif.gif')";
+  inner.style.backgroundSize = "cover";
+  inner.style.borderRadius = "8px";
+
+  el.appendChild(inner);
+
+  new mapboxgl.Marker(el)
+    .setLngLat([13.405, 52.52])
+    .addTo(map);
+
+}, 1000);
 
   }, []);
 
